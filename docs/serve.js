256dd77e49eadf87c2ae36b0439c339fb827cea8
OzgurSeyidoglu <ozgur.seyidoglu.sw@gmail.com>
2020-09-16 11:34:05 +0300

    feat: Added scripts used in components and made it globally avaiable

--- docs/serve.js
+++ docs/serve.js
@@ -3,6 +3,34 @@ import Doc from './serve.vue';
 
 Vue.config.productionTip = false;
 
+const tryGet = (value) => {
+  try {
+    const r = value();
+    if (typeof r !== 'undefined') {
+      if (r === null) {
+        return '';
+      }
+      return r;
+    }
+
+    return '';
+  } catch (e) {
+    return '';
+  }
+};
+
+Vue.prototype.tryGet = tryGet;
+
+const requireIcons = require.context('../src/assets/icons', false, /\.svg$/);
+
+requireIcons.keys().forEach((fileName) => {
+  const iconName = upperFirst(camelCase(fileName.replace(/(\.\/|\.svg)/g, '')));
+
+  const componentConfig = requireIcons(fileName);
+
+  Vue.component(iconName, componentConfig.default || componentConfig);
+});
+
 new Vue({
   render: (h) => h(Doc),
 }).$mount('#app');
