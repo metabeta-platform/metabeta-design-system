46274d56b80cbe7e937da903db670b62a5aa3964
cristianburac <cristian.burac@gmail.com>
2020-09-25 09:16:33 +0300

    feat: MbMenu container and MbMenuItem

---
+++ src/components/cells/MbMenu.vue
@@ -0,0 +1,81 @@
+<template>
+<nav :class="['mb-menu', `mb-size-${size}`, `mb-flow-${flow}`]">
+  <h5 class="mb-menu-title">{{title}}</h5>
+  <ul>
+      <mb-menu-item v-for="link in links" :key="link._id" @click="$router.push({name : link.href})" :href="link.href" :menuItemContent="link.menuItemContent" :isDisabled="link.isDisabled"> 
+      </mb-menu-item>
+  </ul>
+</nav>
+</template>
+
+<script>
+export default {
+  name: 'MbMenu',
+  props: {
+    title:{
+      type: [String, Number],
+    },
+    size:{
+      type: String,
+      default: 'm',
+    },
+    flow:{
+      type: String,
+      default: 'vertical',
+    },
+    links:{
+      type: [Array, Object],
+    }
+  },
+  components: {
+    MbMenuItem : () => import('@/components/cells/MbMenuItem'),
+  },
+}
+</script>
+
+<style lang="scss">
+@import "../../assets/styles/partials/_mb_color.scss";
+@import "../../assets/styles/partials/_mb_space.scss";
+@import "../../assets/styles/partials/_mb_typography.scss";
+.mb-menu{
+  &-title{
+    padding: 0 $mb-space-xs;
+    @include mb-heading(xs);
+    color: $mb-color-text-light;
+  }
+  ul{
+    li{
+    margin:0;
+    padding:0;
+    }
+  }
+  &.mb-flow{
+    &-horizontal{
+      li{
+        display:inline-block;
+        margin-right:$mb-space-xs;
+      }
+    }
+    &-vertical{
+      li{
+        display:flex;
+      }
+    }
+  }
+  &.mb-size{
+    &-m{
+      .mb-menu-title{
+        @include mb-heading(xs);
+        color: $mb-color-text-light;
+      }
+    }
+    &-l{
+      .mb-menu-title{
+        @include mb-heading(s);
+        padding: $mb-space-xs;
+        color: $mb-color-text-light;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/cells/MbMenuItem.vue
@@ -0,0 +1,91 @@
+<template>
+   <li  :class="['mb-menu-item', menuItemDisabled ? 'mb-is-disabled' : null]">
+      <a :href="href"
+      @click="$emit('click', $event)"
+      >
+        {{menuItemContent}}
+      </a>
+    </li>
+</template>
+
+<script>
+export default {
+  name: 'MbMenuItem',
+  props: {
+    menuItemContent: {
+      type: String,
+      default: '',
+    },
+    href: {
+      type: String,
+      default: '',
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false,
+    },
+
+  },
+   computed:{
+    menuItemDisabled:{
+      get(){
+        return this.isDisabled;
+      },
+      set(value){
+        this.isDisabled = value;
+      }
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/styles/partials/_mb_space.scss";
+@import "../../assets/styles/partials/_mb_typography.scss";
+.mb-menu-item{
+  a{
+    width:100%;
+    padding: $mb-space-xs;
+    @include mb-border-radius(s);
+    color: $mb-color-text-medium;
+    outline: none;
+    @include mb-font(body, s, normal, normal);
+    @include mb-wrap-text(false);
+    &:hover{
+      color: $mb-color-headings;
+      background-color: $mb-color-gray-highlight;
+    }
+     &:active{
+      background-color: $mb-color-primary-highlight;
+      color: $mb-color-primary;
+    }
+  }
+  &.mb-is-disabled{
+    a{
+    pointer-events:none;
+    cursor: not-allowed;
+    background-color: fade-out(desaturate($mb-color-gray, $mb-opacity-m),$mb-opacity-xxl);
+    color: fade-out($mb-color-text-medium, $mb-opacity-l);
+    }
+  }
+}
+.mb-menu{
+  &.mb-size{
+    &-m{
+      .mb-menu-item{
+        a{
+         @include mb-font(body, s, normal, normal);
+        }
+      }
+    }
+    &-l{
+      .mb-menu-item{
+      a{
+        @include mb-font(body, m, normal, normal);
+        padding: $mb-space-xs $mb-space-s;
+      }
+    }
+    }
+  }
+}
+</style>
