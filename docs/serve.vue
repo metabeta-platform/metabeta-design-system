895c852f2818b269862abe436541aee2b1e423d0
cristianburac <cristian.burac@gmail.com>
2020-10-27 21:30:04 +0200

    fix: Updated the documentation for MbPanel

--- docs/serve.vue
+++ docs/serve.vue
@@ -2076,7 +2076,7 @@ main {
           <h1>MbPanel</h1>
         </template>
         <template slot="content">
-          <p>Panel description</p>
+          <p>MbPanel component creates a container with 4 slots (title, header, content, footer)</p>
           <h4>How it works</h4>
           <pre>
             <code> 
@@ -2106,39 +2106,11 @@ main {
               </thead>
               <tbody>
                 <tr>
-                  <td>title</td>
-                  <td>string</td>
-                  <td>none</td>
-                  <td>none</td>
-                  <td>Adds a title to the panel</td>
-                </tr>
-                <tr>
-                  <td>name</td>
-                  <td>string</td>
-                  <td>none</td>
-                  <td>none</td>
-                  <td>Renders in markup the id of the panel</td>
-                </tr>
-                <tr>
                   <td>type</td>
                   <td>string</td>
                   <td>base</td>
                   <td>base, card</td>
                   <td>It is used for styling the panel</td>
-                </tr>
-                <tr>
-                  <td>:has-header</td>
-                  <td>boolean</td>
-                  <td>true</td>
-                  <td>true, false</td>
-                  <td>Used for when you want to hide the header slot of the panel</td>
-                </tr>
-                <tr>
-                  <td>:has-footer</td>
-                  <td>boolean</td>
-                  <td>true</td>
-                  <td>true, false</td>
-                  <td>Used for when you want to hide the footer slot of the panel</td>
                 </tr>
               </tbody>
             </table>
@@ -2146,12 +2118,11 @@ main {
           <h4>MbPanel type</h4>
           <h6>Card</h6>
           <mb-panel
-            title="Panel card"
             type="card"
-            name="panel-title"
-            :has-header="true"
-            :has-footer="true"
           >
+            <template slot="title">
+              <h3>Title content</h3>
+            </template>
             <template slot="header">
               Header content
             </template>
@@ -2164,12 +2135,11 @@ main {
           </mb-panel>
           <h6>Base</h6>
           <mb-panel
-            title="Panel base"
             type="base"
-            name="panel-title"
-            :has-header="true"
-            :has-footer="true"
           >
+            <template slot="title">
+              <h3>Title content</h3>
+            </template>
             <template slot="header">
               Header content
             </template>
@@ -2180,13 +2150,13 @@ main {
               Footer content
             </template>
           </mb-panel>
-          <h4>MbPanel :has-header="false" </h4>
+          <h4>MbPanel without header </h4>
           <mb-panel
             type="base"
-            title="Panel Title"
-            name="panel-title-2"
-            :has-header="false"
           >
+            <template slot="title">
+              <h3>Title content</h3>
+            </template>
             <template slot="header">
               Header content
             </template>
@@ -2197,13 +2167,13 @@ main {
               Footer content
             </template>
           </mb-panel>
-          <h4>MbPanel :has-footer="false" </h4>
+          <h4>MbPanel without footer</h4>
           <mb-panel
-            title="Panel Title"
             type="base"
-            name="panel-title-2"
-            :has-footer="false"
           >
+            <template slot="title">
+             <h3>Title content</h3>
+            </template>
             <template slot="header">
               Header content
             </template>
