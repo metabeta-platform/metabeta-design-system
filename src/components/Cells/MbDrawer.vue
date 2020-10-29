b9d6fc16c3d9fe54f431fce4c36af76369a2378d
OzgurSeyidoglu <ozgur.seyidoglu.sw@gmail.com>
2020-10-29 18:13:47 +0300

    fix: mb-drawer is separated so it can be syntactically selected

--- src/components/Cells/MbDrawer.vue
+++ src/components/Cells/MbDrawer.vue
@@ -1,11 +1,8 @@
 <template>
-  <div
-    class="mb-drawer-wrapper"
-    :class="drawerVisibility ? 'mb-is-visible' : 'mb-is-hidden'"
-  >
+  <div :class="drawerVisibility ? 'mb-drawer mb-is-visible' : 'mb-is-hidden'">
     <article
       :id="name"
-      :class="['mb-drawer', `mb-size-${size}`]"
+      :class="['mb-drawer-inner', `mb-size-${size}`]"
     >
       <nav class="mb-drawer-title">
         <mb-button
@@ -15,10 +12,10 @@
         ></mb-button>
         <slot name="title"></slot>
       </nav>
-      <header 
+      <header
         class="mb-drawer-header"
         v-if="hasHeader"
-        >
+      >
         <slot name="header"></slot>
       </header>
       <section
@@ -34,7 +31,6 @@
         <slot name="footer"></slot>
       </footer>
     </article>
-    <div class="mb-drawer-background"></div>
   </div>
 </template>
 
@@ -115,59 +111,28 @@ export default {
 <style lang="scss" scoped>
 @import "../../assets/styles/partials/_mb_motion.scss";
 @import "../../assets/styles/partials/_mb_space.scss";
-.mb-drawer-wrapper {
+.mb-is-hidden {
+  visibility: hidden;
   position: fixed;
+  right: -100vw;
   top: 0;
-  right: 0;
   width: 100vw;
   height: 100vh;
-  max-width: 100vw;
-  max-height: 100vh;
-  overflow: hidden;
-  z-index: 2;
-  &.mb-is-visible {
-    visibility: visible;
-    .mb-drawer-background {
-      transition: opacity $mb-speed-faster $mb-ease-intro;
-      opacity: 1;
-    }
-    .mb-drawer {
-      transition: all $mb-speed-fast $mb-ease-intro;
-      right: 0;
-    }
-  }
-  &.mb-is-hidden {
-    visibility: hidden;
-    .mb-drawer-background {
-      opacity: 0;
-      transition: opacity $mb-speed-faster $mb-ease-outro;
-    }
-    .mb-drawer {
-      right: -100vw;
-      transition: all $mb-speed-fast $mb-ease-outro;
-    }
-  }
-  .mb-drawer-background {
-    background-color: rgba($mb-color-black, $mb-opacity-m);
-    position: absolute;
-    top: 0;
-    right: 0;
-    width: 100vw;
-    height: 100vh;
-  }
-  .mb-drawer {
+  background-color: $mb-color-transparent;
+  .mb-drawer-inner {
     background-color: $mb-color-white;
     position: absolute;
+    right: 0;
     width: 75vw;
     height: 100vh;
     z-index: 3;
-    .mb-drawer-title{
+    .mb-drawer-title {
       display: flex;
       width: 100%;
       padding: $mb-space-xxs;
       vertical-align: middle;
     }
-    .mb-drawer-header{
+    .mb-drawer-header {
       overflow: hidden;
       border-bottom: $mb-border-thin solid $mb-color-border-light;
       padding: $mb-space-s $mb-space-m;
@@ -182,6 +147,72 @@ export default {
       padding: $mb-space-s $mb-space-m;
       bottom: 0;
       width: 100%;
+      background-color: $mb-color-white;
+      border-top: $mb-border-thin solid $mb-color-border-light;
+    }
+    &.mb-size {
+      &-xl {
+        @include mb-shadow();
+        width: 95vw;
+      }
+      &-l {
+        @include mb-shadow();
+        width: 66.66vw;
+      }
+      &-m {
+        @include mb-shadow();
+        width: 50vw;
+      }
+      &-s {
+        @include mb-shadow();
+        width: 33.33vw;
+      }
+    }
+  }
+}
+
+.mb-is-visible {
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  width: 100vw;
+  height: 100vh;
+  max-width: 100vw;
+  max-height: 100vh;
+  overflow: hidden;
+  background-color: rgba($mb-color-black, $mb-opacity-l);
+  z-index: 2;
+  .mb-drawer-inner {
+    background-color: $mb-color-white;
+    position: absolute;
+    right: 0;
+    width: 75vw;
+    height: 100vh;
+    z-index: 3;
+    .mb-drawer-title {
+      display: flex;
+      width: 100%;
+      padding: $mb-space-xxs;
+      vertical-align: middle;
+    }
+    .mb-drawer-header {
+      overflow: hidden;
+      border-bottom: $mb-border-thin solid $mb-color-border-light;
+      padding: $mb-space-s $mb-space-m;
+    }
+    .mb-drawer-content {
+      max-height: 90vh;
+      overflow-y: scroll;
+      padding: $mb-space-s $mb-space-m;
+    }
+    .mb-drawer-footer {
+      position: fixed;
+      padding: $mb-space-s $mb-space-m;
+      bottom: 0;
+      width: 100%;
+      background-color: $mb-color-white;
       border-top: $mb-border-thin solid $mb-color-border-light;
     }
     &.mb-size {
